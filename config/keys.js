eb0033855407ac7b69b26fc766f0ef5b3c2eb429
zhengchengc <zhengchengc@student.unimelb.edu.au>
2019-11-12 00:11:05 +1100

    create JWT

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+/**
+ * Description.
+ *
+ * @author zhengchengc <z@chenzhengcheng.com>
+ * @since 00:01 12 Nov 2019
+ */
+module.exports = {
+    mongoURI: process.env.MONGO_URI,
+    secretOrKey: process.env.SECORKEY
+};
--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,8 @@
 const express = require('express');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const router = express.Router();
 
 // Load User model
@@ -71,7 +73,13 @@ router.post('/login', (req, res) => {
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
                     if (isMatch) {
-                        res.json({msg: 'Success'});
+                        // User Matched
+                        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT payload
+
+                        //Sign Token
+                        jwt.sign(payload, process.env.SECORKEY, { expiresIn: 3600 }, (err, token) => {
+                            res.json({ success: true, token: 'Bearer ' + token})
+                        });
                     } else {
                         return res.status(400).json({password: 'Password incorrect'});
                     }
