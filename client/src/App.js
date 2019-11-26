e1755670ed19fff699394accff4c1c093b14dbab
zhengchengc <z@olichid.com>
2019-11-26 21:09:57 +1100

    protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
@@ -9,6 +9,8 @@ import store from './store';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+
+import PrivateRoute from "./components/common/PrivateRoute";
 
 import './App.css';
 import Register from './components/auth/Register';
@@ -48,7 +50,9 @@ class App extends Component {
               <div className="container">
                 <Route exact path="/register" component={ Register } />
                 <Route exact path="/login" component={ Login } />
-                <Route exact path="/dashboard" component={ Dashboard } />
+                <Switch>
+                  <PrivateRoute exact path="/dashboard" component={ Dashboard } />
+                </Switch>
               </div>
               <Footer />
             </div>
---
+++ client/src/components/common/PrivateRoute.js
@@ -0,0 +1,36 @@
+/**
+ * @desc
+ *
+ * @author zhengchengc <z@olichid.com>
+ *
+ * @since 2019 11 26 20:57
+ */
+
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const PrivateRoute = ({component: Component, auth, ...rest}) => (
+    <Route
+        {...rest}
+        render = {props =>
+            auth.isAuthenticated === true ? (
+                <Component {...props}/>
+            ) : (
+                <Redirect to="/login"/>
+            )
+        }
+    />
+);
+
+PrivateRoute.propTypes = {
+    auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+
+export default connect(mapStateToProps())(PrivateRoute);
