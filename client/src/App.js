f13cb75b3cec705e69e43e01c4073f62743f1f20
zhengchengc <z@olichid.com>
2019-11-26 22:39:53 +1100

    Create profile component & routes

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import './App.css';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/dashboard';
+import CreateProfile from "./components/create-profile/CreateProfile";
 import { clearCurrentProfile } from "./actions/profileActions";
 
 // Check for token
@@ -52,6 +53,9 @@ class App extends Component {
                 <Route exact path="/login" component={ Login } />
                 <Switch>
                   <PrivateRoute exact path="/dashboard" component={ Dashboard } />
+                </Switch>
+                <Switch>
+                  <PrivateRoute exact path="/create-profile" component={ CreateProfile } />
                 </Switch>
               </div>
               <Footer />
